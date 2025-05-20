23a6f5f0ba2e49a1d1770acd9a559367222e8b58
Ehsan Taghavi <EhsanTgv@gmail.com>
2025-05-20 10:07:07 +0300

    Implement SlideToBookButton

--- app/src/main/java/com/taghavi/slidetounlockjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/taghavi/slidetounlockjetpackcompose/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScopeInstance.align
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.taghavi.slidetounlockjetpackcompose.ui.theme.SlideToUnlockJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +51,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SlideToUnlockJetpackComposeTheme {
-        Greeting("Android")
+fun SlideToBookButton(
+    btnText: String,
+    btnTextStyle: TextStyle,
+    outerBtnBackgroundColor: Color,
+    sliderBtnBackgroundColor: Color,
+    @DrawableRes sliderBtnIcon: Int,
+    onBtnSwipe: () -> Unit
+) {
+    val sliderButtonWidthDP = 70.dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp)
+    ){
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(
+                    color = outerBtnBackgroundColor,
+                    shape = RoundedCornerShape(12.dp)
+                )
+        ){
+            Text(
+                text = btnText,
+                style = btnTextStyle,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+        ){
+            SliderButton(
+                sliderBtnWidth = sliderButtonWidthDP,
+                sliderBtnBackgroundColor = sliderBtnBackgroundColor,
+                sliderBtnIcon = sliderBtnIcon,
+            )
+        }
     }
 }
